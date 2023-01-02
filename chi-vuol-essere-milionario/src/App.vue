f3817b71bb87850ab6aa90b52b0903b2da499858
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2023-01-02 20:02:05 +0100

    Sigla intro

--- chi-vuol-essere-milionario/src/App.vue
+++ chi-vuol-essere-milionario/src/App.vue
@@ -3,10 +3,11 @@
     <div v-if="!this.play">
       <MenuIntroComponent @newGame="getValueNewGame" />
     </div>
-    <div v-else>
+    <div v-else style="height: 100vh; overflow-y: hidden;">
       <div v-if="!skipVideo">
         <div class="skip-btn" @click="skip">
-          <i class="fa-solid fa-forward">Skip</i>
+          <i class="fa-solid fa-forward"></i>
+          <h5>Skip</h5>
         </div>
         <video src="@/../public/video/Sigla.mp4" autoplay></video>
       </div>
@@ -37,6 +38,10 @@ export default {
   methods: {
     getValueNewGame(value) {
       this.play = value;
+
+      setTimeout(() => {
+        this.skipVideo = true;
+      }, 19000);
     },
     skip() {
       this.skipVideo = true;
@@ -54,11 +59,41 @@ export default {
 
 body {
   font-family: sans-serif;
-}
+  position: relative;
 
-video {
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
+  .skip-btn {
+    position: absolute;
+    z-index: 10;
+    bottom: 50px;
+    right: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .5;
+    cursor: pointer;
+
+    i{
+      color: #fff;
+      font-size: 2rem;
+      margin-right: 20px;
+    }
+
+    h5{
+      color: #fff;
+      font-size: 1.5rem;
+    }
+
+    &:hover{
+      opacity: 1;
+    }
+  }
+
+  video {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+
+
 }
 </style>
